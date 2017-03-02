259be8cffec0606cf63b6b64a4eb5552b3df3a95
LucasMcL <LMcLaughlin09@gmail.com>
2017-03-02 15:59:01 -0600

    completed assignment

--- flag.js
+++ flag.js
@@ -1,3 +1,22 @@
-const chalk = require('chalk')
+'use strict'
 
-console.log('hi')
+const chalk = require('chalk')
+const STARS_AND_RED = chalk.white.bgBlue(' *'.repeat(7) + ' ') + chalk.bgRed(' '.repeat(35))
+const STARS_AND_WHITE = chalk.white.bgBlue(' ' + ' *'.repeat(6) + '  ') + chalk.bgWhite(' '.repeat(35))
+const LONG_WHITE = chalk.bgWhite(' '.repeat(50))
+const LONG_RED = chalk.bgRed(' '.repeat(50))
+
+console.log(STARS_AND_RED)
+console.log(STARS_AND_WHITE)
+console.log(STARS_AND_RED)
+console.log(STARS_AND_WHITE)
+console.log(STARS_AND_RED)
+console.log(STARS_AND_WHITE)
+console.log(STARS_AND_RED)
+console.log(LONG_WHITE)
+console.log(LONG_RED)
+console.log(LONG_WHITE)
+console.log(LONG_RED)
+console.log(LONG_WHITE)
+console.log(LONG_RED)
+
